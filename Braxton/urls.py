8eebc2fcbae87a447d961efb4c773843d28157d2
Roman <roman.nenakhov08@gmail.com>
2021-10-11 13:43:42 +0300

    online store

---
+++ Braxton/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import include
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path
+
+from Braxton import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('cart/', include(('cart.urls', 'cart'), namespace='cart')),
+    path('orders/', include(('orders.urls', 'orders'), namespace='orders')),
+    path('', include(('shop.urls', 'shop'), namespace='shop')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
